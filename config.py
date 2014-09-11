d53eb4c91b55661af520ce4ea41e5fb358e769b0
Manaenkov Grigoriy <manaenkov.gregory@gmail.com>
2014-09-11 20:12:17 +0300

    added database support, included migration scripts

--- config.py
+++ config.py
@@ -9,3 +9,9 @@ OPENID_PROVIDERS = [
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' },
     { 'name': 'vk.com', 'url': 'http://vkontakteid.ru/'}
     ]
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')    
