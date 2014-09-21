ad9c775a62c9d024358ab3fac16d2c23ee3d607b
Manaenkov Grigoriy <manaenkov.gregory@gmail.com>
2014-09-21 18:36:27 +0300

    lesson 16, added test coverage tool, fix delete bug

--- app/views.py
+++ app/views.py
@@ -40,6 +40,21 @@ def index(page = 1):
 		posts = posts, 
 		form = form)
 
+@app.route('/delete/<int:id>')
+@login_required
+def delete(id):
+	post = Post.query.get(id)
+	if post == None:
+		flash(gettext('Post not found.'))
+		return redirect(url_for('index'))
+	if post.author.id != g.user.id:
+		flash(gettext('You cannot delete this post.'))
+		return redirect(url_for('index'))
+	db.session.delete(post)
+	db.session.commit()
+	flash(gettext('Your post has been deleted'))
+	return redirect(url_for('index'))
+
 @app.route('/login', methods = ['GET', 'POST'])
 @oid.loginhandler
 def login():
--- tests.py
+++ tests.py
@@ -8,6 +8,10 @@ from app import app, db
 from app.models import User, Post
 from datetime import datetime, timedelta
 from app.translate import microsoft_translate
+from coverage import coverage
+
+cov = coverage(branch = True, omit = ['flask/*', 'tests.py'])
+cov.start()
 
 class TestCase(unittest.TestCase):
 	def setUp(self):
@@ -115,5 +119,30 @@ class TestCase(unittest.TestCase):
 		assert microsoft_translate(u'English', 'en', 'ru') == u'английский'
 		assert microsoft_translate(u'Английский', 'ru', 'en') == u'English'
 
+	def test_delete_post(self):
+		# create a user and a post
+		u = User(nickname = 'john', email = 'john@example.com')
+		p = Post(body = 'test post', author = u, timestamp = datetime.utcnow())
+		db.session.add(u)
+		db.session.add(p)
+		db.session.commit()
+		# query the post and destroy the session
+		p = Post.query.get(1)
+		db.session.remove()
+		# delete the post using a new session
+		db.session = db.create_scoped_session()
+		db.session.delete(p)
+		db.session.commit()
+
 if __name__ == '__main__':
-	unittest.main()		
+	try:
+		unittest.main()		
+	except:
+		pass
+	cov.stop()
+	cov.save()
+	print "\n\nCoverage Report:\n"
+	cov.report()
+	print "HTML version: " + os.path.join(basedir, "tmp/coverage/index.html")
+	cov.html_report(directory = 'tmp/coverage')
+	cov.erase()
