8fbb208a2cc08274ac60e50909afb3e425848b3e
Manaenkov Grigoriy <manaenkov.gregory@gmail.com>
2014-09-24 19:33:12 +0300

    added translation to ukrainian

--- tr_init.py
+++ tr_init.py
@@ -6,7 +6,7 @@ if sys.platform == 'win32':
 	pybabel = 'flask\\Scripts\\pybabel'
 else:
 	pybabel = 'flask/bin/pybabel'
-if len(sys.argv != 2):
+if len(sys.argv) != 2:
 	print "usage: tr_init <language-code>"
 	sys.exit(1)
 os.system(pybabel + ' extract -F babel.cfg -k lazy_gettext -o messages.pot app')
